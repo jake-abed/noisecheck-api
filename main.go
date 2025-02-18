685db9e2533724949e26f0595dc8dd47c68bf17c
Jake Abed <jakeabed@proton.me>
2025-02-18 12:24:34 -0800

    Tidy up main file and add notes for posterity

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ type apiConfig struct {
 }
 
 func main() {
+	// Get and set all env variables.
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
@@ -31,7 +32,8 @@ func main() {
 	clerkKey := os.Getenv("CLERK_KEY")
 
 	dbURL := fmt.Sprintf("%s?authToken=%s", tursoUrl, tursoToken)
-
+	
+	// Try to open DB and exit if it won't work.
 	db, err := sql.Open("libsql", dbURL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open db: %s: %s", dbURL, err)
@@ -40,18 +42,29 @@ func main() {
 	defer db.Close()
 
 	dbQueries := database.New(db)
-
 	cfg := apiConfig{Db: dbQueries}
 
+	// Prep handlers with required auth wrapper.
 	createRelease, _ := cfg.createReleaseHandler().(http.Handler)
 	authdCreateRelease, _ := clerkhttp.
 		WithHeaderAuthorization()(createRelease).(http.HandlerFunc)
 	clerk.SetKey(clerkKey)
+
+	// Create Router
 	r := chi.NewRouter()
+
+	// Add Middleware To Router
 	r.Use(middleware.Logger)
-	r.Use(cors.AllowAll().Handler)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedHeaders: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		MaxAge:         300,
+	}))
+
+	// Routes/Handlers
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome\n"))
+		w.Write([]byte("welcome, but this an api bruh\n"))
 	})
 	r.Post("/api/webhooks/users", cfg.userWebhookHandler)
 	r.Post("/api/releases", authdCreateRelease)
